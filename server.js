419217a59b6e3f004216ee15d9bc2b0014a80984
Md afratul kaoser Taohid <taohid32@gmail.com>
2020-12-09 14:42:51 +0600

    password validation solved

--- server.js
+++ server.js
@@ -10,25 +10,31 @@ const bodyParser = require('body-parser')
 const userRoute = require('./app/routes/user_route')
 const app = express()
 
-// const corsOptions = {
-//     origin: '*',
-// }
-// app.use(cors(corsOptions))
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
 // Accept json in express server
 app.use(bodyParser.json({limit: '50mb', extended: true}))
 // Configure bodyparser to handle post requests
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
-
 app.use(session({secret : process.env.TOKEN_SECRET, resave : false, saveUninitialized : true}))
-app.enable("trust proxy")
-app.use(helmet())
+
+const whitelist = ['https://api.letsdunch.com','http://localhost:3000']
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+
+app.enable("trust proxy"); // only if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+app.use(cors(corsOptions));
+app.use(helmet());
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 //add all the http route
 app.use(userRoute)
