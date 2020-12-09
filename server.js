13c8ff82995b25dafe6d6a269c9c17495b29ceaf
Md afratul kaoser Taohid <taohid32@gmail.com>
2020-12-09 14:18:08 +0600

    password validation solved

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ const bodyParser = require('body-parser')
 const userRoute = require('./app/routes/user_route')
 const app = express()
 
-const corsOptions = { origin: true, credentials: true }
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 app.use(cors(corsOptions))
 
 // Accept json in express server
