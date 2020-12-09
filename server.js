fd30adfd1a10cbb8e871cafacc534398036c4297
Md afratul kaoser Taohid <taohid32@gmail.com>
2020-12-09 14:53:09 +0600

    password validation solved

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json({limit: '50mb', extended: true}))
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
 app.use(session({secret : process.env.TOKEN_SECRET, resave : false, saveUninitialized : true}))
 
-const whitelist = ['https://api.letsdunch.com','http://localhost:3000']
+// const whitelist = ['https://api.letsdunch.com','http://localhost:3000']
 // const corsOptions = {
 //     origin: function (origin, callback) {
 //         if (whitelist.indexOf(origin) !== -1) {
@@ -27,14 +27,9 @@ const whitelist = ['https://api.letsdunch.com','http://localhost:3000']
 //     }
 // }
 
-// app.enable("trust proxy"); // only if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+app.enable("trust proxy"); // only if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
 app.use(cors());
 app.use(helmet());
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
 
 //add all the http route
 app.use(userRoute)
